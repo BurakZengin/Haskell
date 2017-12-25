00f1fc6c3862a3bc7bf5d8370bcc83f64b294363
Burak Zengin <brakzengin@gmail.com>
2017-12-25 14:11:30 +0300

    Update file

--- app.hs
+++ app.hs
@@ -1,6 +1,12 @@
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100 then x else x*2
+f xs = [if x<10 then "a" else "b" | x <- xs, odd x ]
+fa xs ys = [x*y | x <- xs, y <- ys]
+length' xs = sum[1 | _ <- xs]
+removeLowerCase st = [c | c <- st, c `elem` ['A'..'Z']]
+removeUpperCase xs = [x | x <- xs, x `elem` ['a'..'z']]
+findEven xss = [ [x | x <- xs, even x] | xs <- xss ]
 
 {--
 
@@ -47,6 +53,16 @@ because it's easier to read that way.
 -----------
 also you can define the list range like this
 [1..20], ['K'..'Z'] or [2,4..,20]
+
+            ----------------------
+Tuples
+
+fst takes a pair and returns its first component.
+-----------
+snd takes a pair and returns its second component.
+-----------
+zip can take two lists that contain different types and zip them up.
+-----------
 
 
 --}
